d11e0508fd300e76b5659c379d601975eac19a1a
Prabin Poudel <15196941+coolprobn@users.noreply.github.com>
2024-10-03 07:21:23 +0545

    Disable browser restrictions for now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  # TODO: find a way to show warning page to users with the option to access the page instead of complete restrictions
+  # allow_browser versions: :modern
 end
