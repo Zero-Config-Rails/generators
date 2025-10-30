5049c8fc365e5c5c948add5fa37a3997aaec7d8d
Prabin Poudel <15196941+coolprobn@users.noreply.github.com>
2025-10-30 12:46:10 +0545

    Remove deprecated enable_tracing from Sentry config

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,7 +1,8 @@
 Sentry.init do |config|
   config.dsn = Rails.application.credentials.dig(:sentry, :dsn_key)
-  # enable performance monitoring
-  config.enable_tracing = true
+
   # get breadcrumbs from logs
   config.breadcrumbs_logger = [ :active_support_logger, :http_logger ]
+  # don't send default PII (personally identifiable information)
+  config.send_default_pii = false
 end
